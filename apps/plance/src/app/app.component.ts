37bec74b0b013889ffc756871e0fd9c1101820b8
ben <contact@benjamincabanes.com>
2021-06-18 16:57:39 -0400

    initial commit

---
+++ apps/plance/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spitting-octo-fire-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'plance';
+}
